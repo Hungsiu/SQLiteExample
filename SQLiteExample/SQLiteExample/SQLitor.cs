3b1934c951c22e1bb8b32d7bb490dd094db04587
Hungsiu <hx.wang.tw@gmail.com>
2024-05-02 16:31:57 +0800

    安裝SQLite套件 System.Data.SQLite

---
+++ SQLiteExample/SQLiteExample/SQLitor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLiteExample
+{
+    public class SQLitor
+    {
+        protected SQLiteConnection connection = new SQLiteConnection();
+
+        protected string LogPath { get; private set; } = "C:\\Logs";
+        protected string LogFile { get; private set; } = "Log.db";
+
+        public void Setup(string logPath="Logs",string logFile="Log.db")
+        {
+            LogPath = Application.StartupPath + "\\" + logPath;
+            LogFile = logFile;
+
+            connection.Open();
+
+        }
+    }
+}
