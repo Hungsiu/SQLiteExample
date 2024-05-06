5ade5e9318387ac96e382fffdb5fb802a6c45c02
Hungsiu <hx.wang.tw@gmail.com>
2024-05-06 16:19:49 +0800

    註解增加SQL與罰 CommandExcute內容移到正確的Setup處

--- SQLiteExample/SQLiteExample/SQLitor.cs
+++ SQLiteExample/SQLiteExample/SQLitor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Data.SQLite;
 using System.Linq;
 using System.Text;
@@ -9,32 +10,52 @@ namespace SQLiteExample
 {
     public class SQLitor
     {
-        protected string DbPath = Application.StartupPath;
+        /*
+         *  SQL語法
+         *      資料型態
+         *      - NULL
+         *      - INTEGER
+         *      - REAL
+         *      - TEXT
+         *      - BLOB
+         *      - DATETIME
+         *  
+         *  
+         *      建立表格
+         *      CREATE TABLE `MyTableName`(
+         *          `First Column` Data-Type,
+         *          `Second Column` Data-Type,
+         *          `Third Column` Data-Type,
+         *          .
+         *          .
+         *          .
+         *          .
+         *          .
+         *          ...
+         *      );
+         * 
+         * 
+         */
+
+
+        protected string DbPath = "Path";
         protected string DbFile = "Data.db";
+        protected string TableName = string.Empty;
+        protected string commandCreateTable = string.Empty;
 
         protected SQLiteConnection connection = new SQLiteConnection();
-        protected string commandCreateDataBase =
-            @"CREATE TABLE IF NOT EXISTS NewDataBase (
-				_AI      INTEGER  PRIMARY KEY AUTOINCREMENT,
-				DateTime DATETIME,
-				Topic    TEXT,
-				Message  TEXT
-			);";
 
-        public SQLitor(string dbPath = "Datas", string dbFile = "Data.db")
+        public SQLitor(string dbPath = "Datas", string dbFile = "Data.db", string tableName = "NewTable")
         {
             DbPath = Application.StartupPath + "\\" + dbPath;
             DbFile = dbFile;
-
+            TableName = tableName;
+            commandCreateTable = "CREATE TABLE IF NOT EXISTS " + TableName + " (_AI INTEGER PRIMARY KEY AUTOINCREMENT,DateTime DATETIME,Topic TEXT,Message TEXT);";
+            
             Setup();
         }
 
         protected void Setup()
-        {
-            connection.Open();
-        }
-
-        public void CommandExcute(string sqlCommand)
         {
             //  DB目錄不存在就建立一個
             if (!Directory.Exists(DbPath))
@@ -44,8 +65,21 @@ namespace SQLiteExample
             if (!File.Exists(DbFile))
                 Directory.CreateDirectory(DbPath + "\\" + DbFile);
 
+            //  SQLite連線處
             var DataSource = DbPath + DbFile;
             connection.ConnectionString = "Data source = " + DataSource;
+
+            connection.Open();
+            CommandExcute(commandCreateTable);
+
+        }
+
+        public void CommandExcute(string sqlCommand)
+        {
+            using (var command = new SQLiteCommand(sqlCommand, connection))
+            {
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
